04544a833941d1ca9608c0f554a6f166ea78c389
Rahul Kadam <rahul-dnyaneshwar_kadam@astro.com.my>
2020-07-03 16:42:52 +0530

    java react code suport and deployment

--- src/main/java/com/apg/payment/Config.java
+++ src/main/java/com/apg/payment/Config.java
@@ -8,20 +8,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class Config implements WebMvcConfigurer {
 
-
-
-    /**
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**")
                 .addResourceLocations("classpath:static");
-    }*/
+    }
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("index.html");
-        registry.addViewController("/view").setViewName("index.html");
-        registry.addViewController("/view1").setViewName("index.html");
+        registry.addViewController("/").setViewName("forward:static/index.html");
+        registry.addViewController("/view").setViewName("forward:static/index.html");
+        registry.addViewController("/view1").setViewName("forward:static/index.html");
     }
 
 }
