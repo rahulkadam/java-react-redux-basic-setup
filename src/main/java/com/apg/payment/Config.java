25fbc19e0b94009cb41524f6c1a60e17825d3933
Rahul Kadam <rahul-dnyaneshwar_kadam@astro.com.my>
2020-07-03 16:04:53 +0530

    adding changes for react folder

--- src/main/java/com/apg/payment/Config.java
+++ src/main/java/com/apg/payment/Config.java
@@ -1,6 +1,7 @@
 package com.apg.payment;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -8,13 +9,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class Config implements WebMvcConfigurer {
 
 
-    /*
+
+    /**
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**")
                 .addResourceLocations("classpath:static");
-    }
-    */
+    }*/
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
